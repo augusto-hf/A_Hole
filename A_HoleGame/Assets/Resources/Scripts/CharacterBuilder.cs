2dcc81b4179e06d9abfb1c3397902211935502a6
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-02-04 14:23:45 -0300

    Starting CharacterBuilding Script

--- A_HoleGame/Assets/Resources/Scripts/CharacterBuilder.cs
+++ A_HoleGame/Assets/Resources/Scripts/CharacterBuilder.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CharacterBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RuleTile wallTile, groundTile;
+    public Tilemap wallTileMap;
+
+    [SerializeField] float castDistance = 1.0f, blockDestroyTime = 0.0f;
+    [SerializeField] LayerMask raycastLayer;
+
+    Vector3 direction;
+    Transform raycastPosition;
+    RaycastHit2D hit;
+
+    bool destroyingBlock = false, placingBlock = false;
+
+    private void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            direction.x = Input.GetAxis("Horizontal");
+            direction.y = Input.GetAxis("Vertical");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+
+    }
+
+    private void RaycastDirection()
+    {
+        hit = Physics2D.Raycast(transform.position, direction, castDistance, raycastLayer.value);
+        Vector2 endpos = raycastPosition.position + direction;
+        Debug.DrawLine(transform.position, hit.point);
+
+        if (Input.GetButton("Interact"))
+        {
+            if (hit.collider && !destroyingBlock)
+            {
+                destroyingBlock = true;
+                StartCoroutine(DestroyBlock(hit.collider.gameObject.GetComponent<Tilemap>(), endpos));
+            }
+        }
+    }
+
+    IEnumerator DestroyBlock(Tilemap map, Vector2 pos)
+    {
+        yield return new WaitForSeconds(blockDestroyTime);
+
+        pos.x = Mathf.Floor(pos.x);
+        pos.y = Mathf.Floor(pos.y);
+
+        map.SetTile(new Vector3Int((int)pos.x, (int)pos.y), null);
+
+        destroyingBlock = true;
+    }
+
+    IEnumerator PlaceBlock(Tilemap map, Vector2 pos)
+    {
+        yield return new WaitForSeconds(0f);
+
+        pos.x = Mathf.Floor(pos.x);
+        pos.y = Mathf.Floor(pos.y);
+
+        map.SetTile(new Vector3Int((int)pos.x, (int)pos.y), wallTile);
+
+        placingBlock = true;
     }
 }
--- A_HoleGame/Assets/Resources/Scripts/CharacterInteractor.cs
+++ A_HoleGame/Assets/Resources/Scripts/CharacterInteractor.cs
@@ -1,49 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class CharacterInteractor : MonoBehaviour
 {
-    public RuleTile wallTile;
-    public Tilemap wallTileMap;
-
-    [SerializeField] float castDistance = 1.0f, blockDestroyTime = 0.0f;
-    [SerializeField] LayerMask raycastLayer;
-
-    Vector2 direction;
-    RaycastHit2D hit;
-
-    bool destroyingBlock = false, placingBlock = false;
-
-    private void Update()
-    {
-        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            direction.x = Input.GetAxis("Horizontal");
-            direction.y = Input.GetAxis("Vertical");
-        }
-
-    }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
-    private void RaycastDirection()
-    {
-        hit = Physics2D.Raycast(transform.position, direction, castDistance, raycastLayer.value);
-        Debug.DrawLine(transform.position, hit.point);
-
-        if(Input.GetButton("Interact"))
-        {
-            if(hit.collider && !destroyingBlock)
-            {
-                destroyingBlock = true;
-            }
-        }
-    }
-
-    IEnumerator DestroyObject(Tilemap )
 }
+
+
