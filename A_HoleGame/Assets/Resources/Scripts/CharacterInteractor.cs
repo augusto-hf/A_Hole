3147146b16b3e7e03dface6d2a99be1a82132daf
augusto-hf <51714609+augusto-hf@users.noreply.github.com>
2023-02-04 13:31:07 -0300

    block breaking

--- A_HoleGame/Assets/Resources/Scripts/CharacterInteractor.cs
+++ A_HoleGame/Assets/Resources/Scripts/CharacterInteractor.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CharacterInteractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RuleTile wallTile;
+    public Tilemap wallTileMap;
+
+    [SerializeField] float castDistance = 1.0f, blockDestroyTime = 0.0f;
+    [SerializeField] LayerMask raycastLayer;
+
+    Vector2 direction;
+    RaycastHit2D hit;
+
+    bool destroyingBlock = false, placingBlock = false;
+
+    private void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            direction.x = Input.GetAxis("Horizontal");
+            direction.y = Input.GetAxis("Vertical");
+        }
+
+    }
+
+    private void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RaycastDirection()
     {
-        
+        hit = Physics2D.Raycast(transform.position, direction, castDistance, raycastLayer.value);
+        Debug.DrawLine(transform.position, hit.point);
+
+        if(Input.GetButton("Interact"))
+        {
+            if(hit.collider && !destroyingBlock)
+            {
+                destroyingBlock = true;
+            }
+        }
     }
+
+    IEnumerator DestroyObject(Tilemap )
 }
